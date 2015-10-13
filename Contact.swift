b585686a900b4594c39b5e32a4629c0a04815897
507diane <s0298794@student.rockvalleycollege.edu>
2015-10-12 20:52:33 -0500

    D

---
+++ Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  MyContact
+//
+//  Created by Diane Christy on 10/12/15.
+//  Copyright © 2015 Diane Christy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Contact: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
